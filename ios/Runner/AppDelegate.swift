ceeb35a85159a475355c1c731b8b53215a878942
apenmoh <167619359+apenmoh@users.noreply.github.com>
2025-05-18 00:06:02 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import Flutter
 import UIKit
-import FirebaseCore
-import SwiftUI
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -9,21 +7,6 @@ import SwiftUI
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    return true
-  }
-}
-@main
-struct YourApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentView()
-      }
-    }
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
